85d40b71dbf2e5261f87c1a8d853e5e6d4cf35c7
Yasas Maddumage <yasasrm@gmail.com>
2022-01-08 17:43:24 +0530

    Project Created

---
+++ src/main/java/com/homesecurity/recoder/App.java
@@ -0,0 +1,13 @@
+package com.homesecurity.recoder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
